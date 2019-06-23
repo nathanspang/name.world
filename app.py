c81087857365a9301cfd139447a1fc745d530984
nathanspang <nathanspang@tamu.edu>
2019-06-23 17:21:19 -0500

    initial setup

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/createaccount')
+def createaccount():
+    return render_template('createaccount.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
